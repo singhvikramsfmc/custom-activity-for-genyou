abf730cdcf4bdc4be03184717eff74ecf11a61ec
singhvikramsfmc <80390771+singhvikramsfmc@users.noreply.github.com>
2021-03-16 13:13:57 +0530

    Update require.js

--- require.js
+++ require.js
@@ -2145,9 +2145,7 @@ var requirejs, require, define;
 }(this, (typeof setTimeout === 'undefined' ? undefined : setTimeout)));
 
 /* Added by SFMC -- BELOW */
-define([
-	"postmonger",
-    "jquery"]
+define(["postmonger"],
   function () {
     define(["main"], function (App) {
            App.start();
